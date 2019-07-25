3cffb30dc12ccba1dc706e780b510b5fe37207ec
谢峻 <jy.xj@alibaba-inc.com>
2019-07-25 17:00:07 +0800

    增加方法

--- src/index.js
+++ src/index.js
@@ -2,6 +2,31 @@ function log(msg) {
     console.log(msg)
 }
 
+function throttle(fn, delay) {
+    let last = +new Date();
+    return ()=> {
+        let cur = +new Date();
+        if(cur - last > delay) {
+            last = +new Date();
+            fn.apply(this, arguments);
+        }
+    }
+}
+
+function debounce(fn, delay) {
+    let timer;
+    return ()=> {
+        clearTimeout(timer);
+        timer = setTimeout(()=> {
+            let ctx = this;
+            let args = arguments;
+            fn.apply(ctx, args)
+        }, delay)
+    }
+}
+
 export {
-    log
+    log,
+    throttle,
+    debounce
 }
